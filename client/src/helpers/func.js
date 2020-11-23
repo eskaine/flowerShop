8b3ddc97918132afa0b591566d7ca0577e28ce24
eskaine <eskaineliu@gmail.com>
2020-11-23 14:56:54 +0800

    update func.js

--- client/src/helpers/func.js
+++ client/src/helpers/func.js
@@ -1,3 +1,11 @@
 export function productUrl(str) {
     return str.split(' ').map(el => el.toLowerCase()).join('-');
 }
+
+export function authHeader(token) {
+    return {
+        headers: {
+            "x-auth-token": token
+          }
+    }
+}
