dae9240fbb49084638363e8a8474e79ace1ecf08
eskaine <eskaineliu@gmail.com>
2020-11-24 22:48:06 +0800

    register/login validation

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -9,23 +9,6 @@ async function createToken(payload) {
     return token;
 }
 
-function checkToken(req, res, next) {
-    const token = req.header("x-auth-token");
-  
-    if (!token) {
-      return res.sendStatus(401);
-    }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded.user;
-      next();
-    } catch (error) {
-      return res.sendStatus(401);
-    }
-  }
-
 module.exports = {
     createToken,
-    checkToken
 };
