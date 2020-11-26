2d095a7050315ea2a878dd0bab02d7fd3ff9bbef
Tyrone <tyronetang.xi@yahoo.com>
2020-11-26 13:00:37 +0800

    updated again

--- client/src/components/contents/private/Checkout.jsx
+++ client/src/components/contents/private/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Card, Accordion, Col, Container, Row, Image, Form } from "react-bootstrap";
+import { Link } from "react-router-dom"
 import { useSelector } from "react-redux";
 import { axiosGet } from "../../../helpers/api";
 
@@ -8,14 +9,46 @@ import { axiosGet } from "../../../helpers/api";
 function Checkout() {
     const user = useSelector(state => state.user);
     const [orders, setOrders] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [accHeader, setAccHeader] = useState(
+      {
+        header1: true,
+        header2: true,
+        header3: true
+      }
+    );
+    
     let shipping = ["Self-pickup", "SingPost", "Ninja-van"];
     
+
+
     async function fetchCheckOut(){
         let url = process.env.REACT_APP_CART + `/${user.id}`;
         let res = await axiosGet(url, user.token);
         setOrders(res.userCart)
-
+        calcCart(res.userCart);
     }
+
+    function calcCart(price){
+      let sum = 0;
+      price.forEach(item=>(
+        sum += item.totalPrice
+      ))
+      setTotal(sum)
+    }
+
+
+    function clickState(e){
+      let id = e.target.id
+      if(id == 1) {  
+        (accHeader.header1 === true) ? setAccHeader({...accHeader, header1: false}) : setAccHeader({...accHeader, header1: true})
+      } else if ( id == 2) {
+        (accHeader.header2 === true) ? setAccHeader({...accHeader, header2: false}) : setAccHeader({...accHeader, header2: true})
+      } else if ( id == 3) {
+        (accHeader.header3 === true) ? setAccHeader({...accHeader, header3: false}) : setAccHeader({...accHeader, header3: true})
+      }
+    }
+
 
 
     useEffect(() => {
@@ -23,19 +56,56 @@ function Checkout() {
     },[])
 
 
-    console.log(user)
+
 
     return (
         <Fragment>
             <Accordion defaultActiveKey="1">
-                    <Accordion.Toggle as={Card.Header} eventKey="0" >
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div><h4>ORDER SUMMARY</h4></div> 
-                            <div><text className="text-muted">click to expand</text></div>
+                    <Accordion.Toggle 
+                    as={Card.Header} 
+                    eventKey="0" 
+                    onClick={clickState}
+                    id={1}
+                    >
+                        <div 
+                        onClick={clickState}
+                        id={1}
+                        className="d-flex justify-content-between align-items-center">
+                            <div 
+                            onClick={clickState}
+                            id={1}>
+                              <h4
+                              onClick={clickState}
+                              id={1}>
+                              ORDER SUMMARY</h4></div> 
+                            <div
+                            onClick={clickState}
+                            id={1}
+                            ><text className="text-muted">{(accHeader.header1 === true) ? "click to expand" : "click to collapse"}</text></div>
                         </div>
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
                         <Card.Body className="border">
+                        <Container className="cart-item p-0">
+                                    <Container className="my-auto no-gutters px-0">
+                                      <Row className="pt-0">
+                                      <Col s={8} md={2} lg={2}>
+                                            </Col>
+                                        <Col s={10} md={10} lg={10}>
+                                          <Row>
+                                            <Col s={10} md={6} lg={6}>
+                                            </Col>
+                                            <Col s={8} md={2} lg={2}>
+                                            </Col>
+                                            <Col s={8} md={4} lg={4} className="p-0 align-items-center">
+                                              <div className=" mb-0"><text className="font-weight-bold">Grand Total:</text><br/> SGD$ {total}</div>
+                                          
+                                            </Col>
+                                          </Row>
+                                        </Col>
+                                      </Row>
+                                    </Container>
+                                  </Container>
                             {orders.map(order=>(
                                             <Container className="cart-item">
                                             <Container className="my-auto">
@@ -90,15 +160,32 @@ function Checkout() {
                                             </Container>
                                           </Container>
                             ))}
-
+                                  
                         </Card.Body>
                     </Accordion.Collapse>
             </Accordion>
             <Accordion defaultActiveKey="1">
-                    <Accordion.Toggle as={Card.Header} eventKey="0" >
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div><h4>DELIVERY ADDRESS - USER INFORMATION</h4></div> 
-                            <div><text className="text-muted">click to expand</text></div>
+                    <Accordion.Toggle 
+                    as={Card.Header} 
+                    eventKey="0" 
+                    onClick={clickState}
+                        id={2}
+                        >
+                        <div 
+                        onClick={clickState}
+                        id={2}
+                        className="d-flex justify-content-between align-items-center">
+                            <div
+                            onClick={clickState}
+                            id={2}
+                            ><h4
+                            onClick={clickState}
+                            id={2}
+                            >DELIVERY ADDRESS - USER INFORMATION</h4></div> 
+                            <div
+                            onClick={clickState}
+                            id={2}
+                            ><text className="text-muted">{(accHeader.header2 === true) ? "click to expand" : "click to collapse"}</text></div>
                         </div>
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
@@ -122,10 +209,27 @@ function Checkout() {
                     </Accordion.Collapse>
             </Accordion>
             <Accordion defaultActiveKey="1" >
-                    <Accordion.Toggle as={Card.Header} eventKey="0" >
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div><h4>SHIPPING OPTIONS</h4></div> 
-                            <div><text className="text-muted">click to expand</text></div>
+                    <Accordion.Toggle 
+                    as={Card.Header} 
+                    eventKey="0" 
+                    onClick={clickState}
+                        id={3}>
+                        <div 
+                        className="d-flex justify-content-between align-items-center"
+                        onClick={clickState}
+                        id={3}
+                        >
+                            <div
+                            onClick={clickState}
+                            id={3}
+                            ><h4
+                            onClick={clickState}
+                            id={3}
+                            >SHIPPING OPTIONS</h4></div> 
+                            <div
+                            onClick={clickState}
+                            id={3}
+                            ><text className="text-muted">{(accHeader.header3 === true) ? "click to expand" : "click to collapse"}</text></div>
                         </div>
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey="0">
@@ -154,6 +258,17 @@ function Checkout() {
                         </Card.Body>
                     </Accordion.Collapse>
             </Accordion>
+            <Row className="mt-5">
+          <Col s={12} md={5} className="d-flex justify justify-content-center">
+          </Col>
+          <Col s={12} md={7} className="d-flex justify-content-center">
+            <Link
+              className="button"
+            >
+              Place Order
+            </Link>
+          </Col>
+        </Row>
         </Fragment>
     )
 };
